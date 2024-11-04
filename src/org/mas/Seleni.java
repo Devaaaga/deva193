c0873ace788c629a9568b6508fc5e59b81814873
Devaaaga <devaagar914@gmail.com>
2024-11-04 13:48:14 +0530

    done

---
+++ src/org/mas/Seleni.java
@@ -0,0 +1,11 @@
+package org.mas;
+
+public class Seleni {
+
+public void tamilReligion() {
+	System.out.println("tamilreligion:Beautiful");
+}
+public void tamilfestivals() {
+	System.out.println("tamilfestival:Pongal");
+}
+}
